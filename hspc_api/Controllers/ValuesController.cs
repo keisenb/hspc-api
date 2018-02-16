0736fbc1817455c5ff2174f44b84dc881e33b9a1
Kyle Eisenbarger <keisenb@ksu.edu>
2018-02-15 20:43:39 -0600

    Basic auth, basic dbcontext, project structure

---
+++ hspc_api/Controllers/ValuesController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using hspc_api.Contracts;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace hspc_api.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        IArticleService articleService;
+        private readonly UserManager<IdentityUser> _userManager;
+
+
+        public ValuesController(IArticleService articleService, UserManager<IdentityUser> userManager) {
+            this.articleService = articleService;
+            _userManager = userManager;
+        }
+
+        [Authorize]
+        [HttpGet]
+        public async Task<object> Protected()
+        {
+            var user = await _userManager.GetUserAsync(HttpContext.User);
+            return new JsonResult(user);
+        }
+    }
+}
---
+++ hspc_api/Data/UserDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace hspc_api.Data
+{
+    public class UserDbContext : IdentityDbContext<IdentityUser>
+    {
+        public UserDbContext(DbContextOptions<UserDbContext> options)
+                : base(options)
+        {
+        }
+    }
+}
